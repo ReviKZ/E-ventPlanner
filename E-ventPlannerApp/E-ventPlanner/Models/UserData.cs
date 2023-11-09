76c85912472a14b87ad7626ca7266532040b2785
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-09 08:15:24 +0100

    Add: Create Models for EF

---
+++ E-ventPlannerApp/E-ventPlanner/Models/UserData.cs
@@ -0,0 +1,12 @@
+﻿using E_ventPlanner.Enums;
+
+namespace E_ventPlanner.Models;
+
+public class UserData
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Gender Gender { get; set; }
+    public Role Role { get; set; }
+}
