e75dbef210104e13f8b0350f63aaaabd20f9a329
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 08:10:03 +0100

    Add: Create AuthController Add: Create Initial Register endpoint

---
+++ E-ventPlannerApp/E-ventPlanner/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_ventPlanner.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("register")]
+        public async Task<bool> Register()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
