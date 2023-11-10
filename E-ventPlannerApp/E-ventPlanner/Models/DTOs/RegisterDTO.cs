4a542e8c9933c09f9ebcb20e1bc0a4242b74b9ab
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 08:16:14 +0100

    Add: Create RegisterDTO

---
+++ E-ventPlannerApp/E-ventPlanner/Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using E_ventPlanner.Enums;
+
+namespace E_ventPlanner.Models.DTOs;
+
+public class RegisterDTO
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Gender Gender { get; set; }
+}
