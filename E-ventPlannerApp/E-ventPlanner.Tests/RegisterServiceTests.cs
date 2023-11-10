5a000ac003be1f8d70ae0220ea185631d9851962
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 10:27:28 +0100

    Add: Create Test Project

---
+++ E-ventPlannerApp/E-ventPlanner.Tests/RegisterServiceTests.cs
@@ -0,0 +1,19 @@
+namespace E_ventPlanner.Tests
+{
+    public class RegisterServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
