2728951c9686c5442c7ec782f3587fd2bd53fdf8
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 08:56:16 +0100

    Add: Create RegisterService with it's dependencies & logic

---
+++ E-ventPlannerApp/E-ventPlanner/Services/RegisterService.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Serialization;
+using E_ventPlanner.Contexts;
+using E_ventPlanner.Enums;
+using E_ventPlanner.Models;
+using E_ventPlanner.Models.DTOs;
+using E_ventPlanner.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace E_ventPlanner.Services;
+
+public class RegisterService : IRegisterService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly PlannerDbContext _db;
+    public RegisterService(UserManager<IdentityUser> userManager, PlannerDbContext db)
+    {
+        _userManager = userManager;
+        _db = db;
+    }
+    public async Task<bool> RegisterUser(RegisterDTO user)
+    {
+        var identityUser = new IdentityUser
+        {
+            UserName = user.Email,
+            Email = user.Email
+        };
+
+        var result = await _userManager.CreateAsync(identityUser, user.Password);
+
+        if (result.Succeeded)
+        {
+            var userId = Int32.Parse(_userManager.Users.First(u => u.Email == user.Email).Id);
+            var userData = new UserData
+            {
+                UserId = userId,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Gender = user.Gender,
+                Role = Role.User
+            };
+            await _db.UserData.AddAsync(userData);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        return false;
+    }
+}
