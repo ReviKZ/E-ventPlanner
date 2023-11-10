1aaa01465791388616a8ee0fea49ffa44c7e9bad
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 09:44:09 +0100

    Add: Register Identity & RegisterService services

--- E-ventPlannerApp/E-ventPlanner/Program.cs
+++ E-ventPlannerApp/E-ventPlanner/Program.cs
@@ -1,4 +1,7 @@
 using E_ventPlanner.Contexts;
+using E_ventPlanner.Services;
+using E_ventPlanner.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +11,18 @@ builder.Services.AddDbContext<PlannerDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        options.Password.RequiredLength = 5;
+        options.Password.RequireNonAlphanumeric = false; //Of course these should be true in a working App for better safety.
+        options.Password.RequireDigit = false;
+        options.Password.RequireUppercase= false;
+        options.User.RequireUniqueEmail = true;
+    }).AddEntityFrameworkStores<PlannerDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddTransient<IRegisterService, RegisterService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
