0456fdebfe7f30383d3897bec050397c11273e98
ReviKZ <kovacs.zoard.1@gmail.com>
2023-11-10 09:39:51 +0100

    Update: Change UserId field type from int to string

--- E-ventPlannerApp/E-ventPlanner/Models/UserData.cs
+++ E-ventPlannerApp/E-ventPlanner/Models/UserData.cs
@@ -4,7 +4,7 @@ namespace E_ventPlanner.Models;
 
 public class UserData
 {
-    public int UserId { get; set; }
+    public string UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public Gender Gender { get; set; }
